5ce8038303e9db5c163cf9ef4593287e41b3367f
jlb86340 <math86340@orange.fr>
2023-12-20 11:45:19 +0100

    add all

---
+++ CommandLineLib/command.cpp
@@ -0,0 +1,6 @@
+#include "pch.h"
+#include "command.h"
+
+command::command(std::string name, std::string alias, std::string description, size_t argumentNumber, bool obligatoryFlag, bool immediatlyFlag)
+{
+}
---
+++ CommandLineLib/command.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include <vector>
+class command
+{
+private:
+	std::string name;
+	std::string alias;
+
+	size_t argumentNumber;
+	std::string description;
+	bool obligatoryFlag;
+	bool immediatlyFlag;
+public:
+	command(std::string name, std::string alias, std::string description, size_t argumentNumber, bool obligatoryFlag = false, bool immediatlyFlag = false);
+	virtual void assignment(std::vector<std::string> assign) const = 0;
+	virtual void launchCommand(std::vector<std::string> command) const = 0;
+};
