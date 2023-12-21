28a44a0665a2562ce239135134130f7cd2a69788
jlb86340 <math86340@orange.fr>
2023-12-21 16:50:36 +0100

    add target file

--- CommandLineLib/target.cpp
+++ CommandLineLib/target.cpp
@@ -1,2 +1,26 @@
 #include "pch.h"
 #include "target.h"
+
+target::target(std::vector<std::string> targs, std::string desc, bool isE): 
+	targets(targs), description(desc), empty(isE)
+{
+}
+
+target::~target()
+{
+}
+
+std::vector<std::string> target::getTargets()
+{
+	return targets;
+}
+
+std::string target::getDescription()
+{
+	return description;
+}
+
+bool target::isEmpty()
+{
+	return empty;
+}
--- CommandLineLib/target.h
+++ CommandLineLib/target.h
@@ -1,6 +1,20 @@
 #pragma once
+#include <vector>
+#include <string>
+
 class target
 {
-
+private:
+	std::vector<std::string> targets;
+	std::string description;
+	bool empty;
+public:
+	target(std::vector<std::string> targs,
+		std::string desc = "",
+		bool empty = true);
+	~target();
+	std::vector<std::string> getTargets();
+	std::string getDescription();
+	bool isEmpty();
 };
 
